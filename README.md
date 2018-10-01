ed0a8164b7ec530bde46a3784a67daafaad0adea
Sudhanshu <sudhanshu11a@gmail.com>
2018-10-01 11:50:01 +0530

    Application skeleton
     Application skeleton structure using maven and spring boot

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# user-notes-management
-ESTful API for a “notes” application using Spring Boot. Used a multilayered architecture and MySql as your database to store the note.
+# gotpoint-assignment-user-notes-management
---
+++ src/main/java/org/gotprint/assignment/usernotesmanagement/UserNotesManagementApplication.java
@@ -0,0 +1,12 @@
+package org.gotprint.assignment.usernotesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotesManagementApplication.class, args);
+	}
+}
